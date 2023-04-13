a91a68244ae6a51cfe0f8f961637d94f82b07cae
FernandoPLP <rm99919@fiap.com.br>
2023-04-13 10:25:49 -0300

    Manipulando h1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Manipulando H1
+console.log("olá")
+const h1Element = document.getElementById("Titulo")
+   
+h1Element.addEventListener("mouseover", function(){ 
+    h1Element.style.backgroundColor = "#ff0000" 
+}) 
+    
+h1Element.addEventListener("mouseleave", function(){ 
+    h1Element.style.backgroundColor = "#fff" 
+})
