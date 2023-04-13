2bd99a529459e34beaadd4024e569392fa81abd9
M4thz12 <rm98832@fiap.com.br>
2023-04-13 10:45:24 -0300

    matheus

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var paragrafo = document.getElementById("para");
+console.log(paragrafo)
+
+paragrafo.addEventListener("mouseleave", ()=>{
+         paragrafo.textContent = "problema1"
+     })
+    paragrafo.addEventListener("mouseover", ()=>{
+       paragrafo.textContent = "A falta de segurança durante o período noturno é uma preocupação comum em muitas cidades, especialmente quando se trata de sinalização inadequada e pouca iluminação. A falta de sincronização dos semáforos é um dos principais problemas que afeta o tráfego noturno, o que pode levar a congestionamentos desnecessários e aumentar o risco de acidentes. Além disso, a dificuldade para atravessar as ruas com segurança, especialmente em áreas com pouca iluminação, pode aumentar a vulnerabilidade dos pedestres a roubos e assaltos."
+    })
+
+    var paragrafo2 = document.getElementById("p2");
+console.log(paragrafo2)
+
+    paragrafo2.addEventListener("mouseleave", ()=>{
+        paragrafo2.textContent = "problema2"
+    })
+   paragrafo2.addEventListener("mouseover", ()=>{
+      paragrafo2.textContent = "Infelizmente, o número de assaltos que ocorrem devido a essa má sinalização é alto em muitas regiões. Sem uma sinalização adequada, muitos pedestres ficam expostos a situações de risco, especialmente durante a noite, quando há pouca movimentação nas ruas. A falta de iluminação também pode contribuir para aumentar a vulnerabilidade dos pedestres a crimes."
+   })
+
+
+   var paragrafo3 = document.getElementById("p3");
+console.log(paragrafo3)
+
+    paragrafo3.addEventListener("mouseleave", ()=>{
+        paragrafo3.textContent = "problema3"
+    })
+   paragrafo3.addEventListener("mouseover", ()=>{
+      paragrafo3.textContent = "Outro problema é a falta de segurança em situações de emergência. Sem um sistema de alerta eficiente, pode ser difícil para as autoridades locais responder rapidamente a acidentes ou outras situações de risco."
+   })
+
+
+
+   var paragrafo4 = document.getElementById("p4");
+console.log(paragrafo4)
+
+    paragrafo4.addEventListener("mouseleave", ()=>{
+        paragrafo4.textContent = "problema4"
+    })
+   paragrafo4.addEventListener("mouseover", ()=>{
+      paragrafo4.textContent = "Em resumo, a falta de segurança e eficiência no tráfego noturno pode afetar a qualidade de vida das pessoas e aumentar o risco de acidentes e assaltos. É importante buscar soluções inovadoras e tecnológicas para resolver esses problemas e garantir a segurança e o bem-estar das comunidades locais."
+   })
+
